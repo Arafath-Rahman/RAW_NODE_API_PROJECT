b2790015f16aaeeaef4271d2314a8c8f3db3aad5
Arafath-Rahman <mkhan172093@bscse.uiu.ac.bd>
2021-03-19 02:33:20 +0600

    Basic structure of index.js added (created a server)

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-/*  Title:
- *
- *
- *
- *
+/*
+ * Title: Uptime Monitoring Application
+ * Description: A RESTFul API to monitor up/down time of user defined links
+ * Author: Md. Arafath Rahman Khan
+ * Date: 19/03/2021
  */
+
+// dependencies
+const http = require('http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, () => {
+        console.log(`listening to port ${app.config.port}...`);
+    });
+};
+
+// handle Request Response
+app.handleReqRes = (req, res) => {
+    // handle response
+    res.end('Hello from the server!');
+};
+
+app.createServer();
